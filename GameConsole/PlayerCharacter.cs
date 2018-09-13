93238c547dc96abc13d4de8f28bb58f7c667ebfe
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-13 16:51:07 +0300

    Add project files.

---
+++ GameConsole/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole {
+    public class PlayerCharacter {
+
+    //    public PlayerCharacter() {
+    //        DateOfBirth = null;
+    //        DaySinceLastLogin = null;
+     //   }
+        public string Name { get; set; }
+        public int? DaySinceLastLogin { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool? IsNew { get; set; }
+
+    }
+}
---
+++ GameConsole/PlayerDisplayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole {
+    public class PlayerDisplayer {
+        public static void Write(PlayerCharacter player) {
+
+            int days = player.DaySinceLastLogin.GetValueOrDefault(-1);
+            Console.WriteLine($"{days} since last login");
+
+            if (string.IsNullOrWhiteSpace(player.Name)) {
+                Console.WriteLine("Player name is null or whitespaces");
+            }
+            else {
+                Console.WriteLine(player.Name);
+            }
+
+            //if (player.DaySinceLastLogin.HasValue) {
+            //    Console.WriteLine(player.DaySinceLastLogin.Value);
+            //}
+            //else {
+            //    Console.WriteLine("No value for DaysSinceLastLogin");
+            //}
+
+            if (player.DateOfBirth.HasValue) {
+                Console.WriteLine(player.DateOfBirth);
+            }
+            else {
+                Console.WriteLine("No date of birth specified");
+            }
+
+            if (player.IsNew == null) {
+                Console.WriteLine("Player newbie status is unwknown");
+            }
+            else if (player.IsNew == true) {
+                Console.WriteLine("Player is a newbie");
+            }
+            else {
+                Console.WriteLine("Player is a expirienced");
+            }
+        }
+    }
+}
