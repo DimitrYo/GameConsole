8d6d1b760d72196d5ac9b016bcdf34dcdde04472
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-13 17:04:31 +0300

    Null-conditional operator.

--- GameConsole/PlayerDisplayer.cs
+++ GameConsole/PlayerDisplayer.cs
@@ -18,7 +18,8 @@ namespace GameConsole {
             }
 
             // int days = player.DaySinceLastLogin.GetValueOrDefault(-1);
-            int days = player.DaySinceLastLogin.HasValue ? player.DaySinceLastLogin.Value : -1;
+            // int days = player.DaySinceLastLogin.HasValue ? player.DaySinceLastLogin.Value : -1;
+            int days = player.DaySinceLastLogin ?? -1;
             Console.WriteLine($"{days} since last login");
 
             //if (player.DaySinceLastLogin.HasValue) {
--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -7,11 +7,39 @@ using System.Threading.Tasks;
 namespace GameConsole {
     class Program {
         static void Main(string[] args) {
-            var player = new PlayerCharacter();
-            player.Name = null;
-            player.DaySinceLastLogin = 42;
+            //var player = new PlayerCharacter();
 
-            PlayerDisplayer.Write(player);
+            //player.DaySinceLastLogin = 42;
+            // PlayerDisplayer.Write(player);
+
+            // int days = player.DaySinceLastLogin.Value;
+
+
+            // PlayerCharacter player = null;
+
+            // int days = player?.DaySinceLastLogin ?? -1;
+
+            // int days;
+            //if (player != null) {
+            //    days = player.DaySinceLastLogin ?? -1;
+            //}
+            //else {
+            //    days = -1;
+            //}
+
+            // Console.WriteLine(days);
+
+
+            PlayerCharacter[] players = new PlayerCharacter[3] {
+                new PlayerCharacter { Name = "Sarah"},
+                new PlayerCharacter{ },
+                null
+            };
+
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
+
             Console.ReadLine();
         }
     }
